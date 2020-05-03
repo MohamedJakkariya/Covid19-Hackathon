f8fa81c6e2143468f056e40c567fbbb6463e044d
Md Jack <mdjac@mail.fenixaubit.com>
2020-05-03 13:01:58 +0530

    completely change to server

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/html/index.html')
+});
+
 app.listen(4000, () => {
   console.log('Server running on port #4000');
 });
